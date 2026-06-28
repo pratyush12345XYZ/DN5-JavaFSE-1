a87b4da61bac4a3755124f232b2f2a8d6cf702a1
Pratyush Kumar <pratyushmohanty12345@gmail.com>
2026-06-28 17:02:18 +0530

    Completed Mockito Advanced Exercise 3 - Mocking Service Dependency in Integration Test

---
+++ Week1/Mockito/Mockito_Advanced exercises/Exercise3_MockingServiceDependencyInIntegrationTest/src/test/java/com/cognizant/mockitoadvanced/com/cognizant/UserIntegrationTest.java
@@ -0,0 +1,45 @@
+package com.cognizant.mockitoadvanced;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserService userService;
+
+    @Test
+    public void testGetUser() throws Exception {
+
+        User user = new User(1L, "Pratyush Kumar Mohanty");
+
+        when(userService.getUserById(1L))
+                .thenReturn(user);
+
+        mockMvc.perform(get("/users/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.name")
+                        .value("Pratyush Kumar Mohanty"));
+
+    }
+
+}
