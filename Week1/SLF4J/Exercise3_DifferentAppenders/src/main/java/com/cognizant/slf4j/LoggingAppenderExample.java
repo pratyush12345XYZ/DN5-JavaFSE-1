2f74be07f4b389f3a6ebeb325a43c356d3387f2e
Pratyush Kumar <pratyushmohanty12345@gmail.com>
2026-06-30 23:10:52 +0530

    Completed SLF4J Exercise 3 - Using Different Appenders

---
+++ Week1/SLF4J/Exercise3_DifferentAppenders/src/main/java/com/cognizant/slf4j/LoggingAppenderExample.java
@@ -0,0 +1,23 @@
+package com.cognizant.slf4j;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LoggingAppenderExample {
+
+    private static final Logger logger =
+            LoggerFactory.getLogger(LoggingAppenderExample.class);
+
+    public static void main(String[] args) {
+
+        logger.debug("This is a debug message.");
+
+        logger.info("This is an info message.");
+
+        logger.warn("This is a warning message.");
+
+        logger.error("This is an error message.");
+
+    }
+
+}
