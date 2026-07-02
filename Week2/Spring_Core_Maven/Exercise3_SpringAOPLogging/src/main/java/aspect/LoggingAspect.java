518f45ffe6ccdac81dbb73a6290fb59601ac5b95
Pratyush Kumar <pratyushmohanty12345@gmail.com>
2026-07-02 22:24:55 +0530

    Completed Exercise 3 - Implementing Logging with Spring AOP

---
+++ Week2/Spring_Core_Maven/Exercise3_SpringAOPLogging/src/main/java/aspect/LoggingAspect.java
@@ -0,0 +1,23 @@
+package com.library.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+
+public class LoggingAspect {
+
+    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+
+        long startTime = System.currentTimeMillis();
+
+        Object result = joinPoint.proceed();
+
+        long endTime = System.currentTimeMillis();
+
+        System.out.println(joinPoint.getSignature().getName()
+                + " executed in "
+                + (endTime - startTime)
+                + " ms");
+
+        return result;
+    }
+
+}
