f223100cad5d2825e860bc918ea051ba70a10939
Pratyush Kumar <pratyushmohanty12345@gmail.com>
2026-07-02 23:04:11 +0530

    Completed Exercise 8 - Implementing Basic AOP with Spring

---
+++ Week2/Spring_Core_Maven/Exercise8_BasicSpringAOP/src/main/java/aspect/LoggingAspect.java
@@ -0,0 +1,13 @@
+package com.library.aspect;
+
+public class LoggingAspect {
+
+    public void beforeAdvice() {
+        System.out.println("Before method execution...");
+    }
+
+    public void afterAdvice() {
+        System.out.println("After method execution...");
+    }
+
+}
