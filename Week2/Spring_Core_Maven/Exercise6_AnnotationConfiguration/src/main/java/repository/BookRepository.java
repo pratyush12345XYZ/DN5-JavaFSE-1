bd59a58d41fb0e0ad5434343f5835b062ab67c25
Pratyush Kumar <pratyushmohanty12345@gmail.com>
2026-07-02 22:52:09 +0530

    Completed Exercise 6 - Configuring Beans with Annotations

---
+++ Week2/Spring_Core_Maven/Exercise6_AnnotationConfiguration/src/main/java/repository/BookRepository.java
@@ -0,0 +1,12 @@
+package com.library.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BookRepository {
+
+    public void displayRepository() {
+        System.out.println("BookRepository bean configured using @Repository annotation.");
+    }
+
+}
