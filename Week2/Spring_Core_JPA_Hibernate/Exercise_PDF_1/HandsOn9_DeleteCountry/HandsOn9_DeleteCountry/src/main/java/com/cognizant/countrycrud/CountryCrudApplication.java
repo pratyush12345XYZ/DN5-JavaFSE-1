10c1075620cf91bf23db138dfaca4f7c006a1086
Pratyush Kumar <pratyushmohanty12345@gmail.com>
2026-07-08 00:00:44 +0530

    Completed Hands-on 9 - Delete Country using Spring Data JPA

---
+++ Week2/Spring_Core_JPA_Hibernate/Exercise_PDF_1/HandsOn9_DeleteCountry/HandsOn9_DeleteCountry/src/main/java/com/cognizant/countrycrud/CountryCrudApplication.java
@@ -0,0 +1,29 @@
+package com.cognizant.countrycrud;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.cognizant.countrycrud.model.Country;
+import com.cognizant.countrycrud.service.CountryService;
+
+@SpringBootApplication
+public class CountryCrudApplication implements CommandLineRunner {
+
+    @Autowired
+    private CountryService countryService;
+
+    public static void main(String[] args) {
+        SpringApplication.run(CountryCrudApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        countryService.deleteCountry("ZZ");
+
+        System.out.println("Country deleted successfully.");
+
+    }
+}
---
+++ Week2/Spring_Core_JPA_Hibernate/Exercise_PDF_1/HandsOn9_DeleteCountry/HandsOn9_DeleteCountry/src/main/java/com/cognizant/countrycrud/service/CountryService.java
@@ -0,0 +1,62 @@
+package com.cognizant.countrycrud.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.cognizant.countrycrud.model.Country;
+import com.cognizant.countrycrud.repository.CountryRepository;
+import com.cognizant.countrycrud.service.exception.CountryNotFoundException;
+
+@Service
+public class CountryService {
+
+    @Autowired
+    private CountryRepository countryRepository;
+
+    public List<Country> getAllCountries() {
+        return countryRepository.findAll();
+    }
+
+    @Transactional
+    public Country findCountryByCode(String countryCode)
+            throws CountryNotFoundException {
+
+        Optional<Country> result = countryRepository.findById(countryCode);
+
+        if (!result.isPresent()) {
+            throw new CountryNotFoundException(
+                    "Country not found with code: " + countryCode);
+        }
+
+        Country country = result.get();
+        return country;
+    }
+    @Transactional
+    public void addCountry(Country country) {
+        countryRepository.save(country);
+    }
+    @Transactional
+    public void updateCountry(String code, String name)
+            throws CountryNotFoundException {
+
+        Optional<Country> result = countryRepository.findById(code);
+
+        if (!result.isPresent()) {
+            throw new CountryNotFoundException(
+                    "Country not found with code: " + code);
+        }
+
+        Country country = result.get();
+        country.setName(name);
+
+        countryRepository.save(country);
+    }
+    @Transactional
+    public void deleteCountry(String code) {
+        countryRepository.deleteById(code);
+    }
+}
