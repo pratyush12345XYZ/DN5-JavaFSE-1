6fed3c217bb464493afa43444d20cbf9d7025c46
Pratyush Kumar <pratyushmohanty12345@gmail.com>
2026-07-08 19:59:34 +0530

    Completed Hands-on 5 - One-to-Many Mapping between Employee and Department

---
+++ Week2/Spring_Core_JPA_Hibernate/2. spring-data-jpa-handson/HandsOn5_OneToMany_Employee_Department/src/main/java/com/cognizant/countrycrud/CountryCrudApplication.java
@@ -0,0 +1,84 @@
+package com.cognizant.countrycrud;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.cognizant.countrycrud.model.Department;
+import com.cognizant.countrycrud.model.Employee;
+import com.cognizant.countrycrud.service.DepartmentService;
+import com.cognizant.countrycrud.service.EmployeeService;
+import com.cognizant.countrycrud.service.SkillService;
+
+@SpringBootApplication
+public class CountryCrudApplication {
+
+    private static EmployeeService employeeService;
+    private static DepartmentService departmentService;
+    private static SkillService skillService;
+
+    public static void main(String[] args) {
+
+        ApplicationContext context =
+                SpringApplication.run(CountryCrudApplication.class, args);
+
+        employeeService = context.getBean(EmployeeService.class);
+        departmentService = context.getBean(DepartmentService.class);
+        skillService = context.getBean(SkillService.class);
+
+        testGetDepartment();
+
+        // testGetEmployee();
+        // testAddEmployee();
+        // testUpdateEmployee();
+    }
+
+    private static void testGetDepartment() {
+
+        System.out.println("========== Get Department ==========");
+
+        Department department = departmentService.get(1);
+
+        System.out.println(department);
+
+        System.out.println(department.getEmployeeList());
+    }
+
+    private static void testAddEmployee() {
+
+        System.out.println("========== Add Employee ==========");
+
+        Employee employee = new Employee();
+
+        employee.setName("Amit");
+        employee.setSalary(new BigDecimal("60000"));
+        employee.setPermanent(true);
+        employee.setDateOfBirth(Date.valueOf("2000-05-15"));
+
+        Department department = departmentService.get(1);
+
+        employee.setDepartment(department);
+
+        employeeService.save(employee);
+
+        System.out.println(employee);
+    }
+
+    private static void testUpdateEmployee() {
+
+        System.out.println("========== Update Employee ==========");
+
+        Employee employee = employeeService.get(1);
+
+        Department department = departmentService.get(3);
+
+        employee.setDepartment(department);
+
+        employeeService.save(employee);
+
+        System.out.println(employee);
+    }
+}
