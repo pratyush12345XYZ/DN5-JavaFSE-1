61f2bb3332a9f4975b457ff1332a63169e2f56e3
Pratyush Kumar <pratyushmohanty12345@gmail.com>
2026-07-09 14:11:10 +0530

    Completed Exercise 3 - Employee Management System Creating Repositories

---
+++ Week2/Spring_Core_JPA_Hibernate/Spring-Data-JPA-and-Hibernate/Exercise3_ EmployeeManagement_SystemCreating_Repositories/src/main/java/com/cognizant/employeemanagementsystem/repository/DepartmentRepository.java
@@ -0,0 +1,12 @@
+package com.cognizant.employeemanagementsystem.repository;
+
+import com.cognizant.employeemanagementsystem.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+
+    List<Department> findByName(String name);
+
+}
