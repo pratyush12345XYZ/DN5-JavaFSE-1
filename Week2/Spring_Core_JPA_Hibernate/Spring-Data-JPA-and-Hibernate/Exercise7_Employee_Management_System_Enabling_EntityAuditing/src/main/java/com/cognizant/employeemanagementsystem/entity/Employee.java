5f19155fbf26c0c94abe44b731fa83dd928cbd93
Pratyush Kumar <pratyushmohanty12345@gmail.com>
2026-07-12 00:21:44 +0530

    Completed Exercise 7 - Employee Management System Enabling Entity Auditing

---
+++ Week2/Spring_Core_JPA_Hibernate/Spring-Data-JPA-and-Hibernate/Exercise7_Employee_Management_System_Enabling_EntityAuditing/src/main/java/com/cognizant/employeemanagementsystem/entity/Employee.java
@@ -0,0 +1,118 @@
+package com.cognizant.employeemanagementsystem.entity;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.NamedQueries;
+import jakarta.persistence.NamedQuery;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "employee")
+@EntityListeners(AuditingEntityListener.class)
+@NamedQueries({
+    @NamedQuery(
+        name = "Employee.findByEmailNamed",
+        query = "SELECT e FROM Employee e WHERE e.email = :email"
+    ),
+    @NamedQuery(
+        name = "Employee.findByNameNamed",
+        query = "SELECT e FROM Employee e WHERE e.name = :name"
+    )
+})
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    @ManyToOne
+    @JoinColumn(name = "department_id")
+    private Department department;
+
+    @CreatedBy
+    private String createdBy;
+
+    @LastModifiedBy
+    private String lastModifiedBy;
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
+
+    public Employee() {
+    }
+
+    public Employee(Long id, String name, String email, Department department) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.department = department;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public String getLastModifiedBy() {
+        return lastModifiedBy;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public LocalDateTime getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+}
