f6a18d1c2cb15c29d9dd8fc81c2f369beeebebab
Pratyush Kumar <pratyushmohanty12345@gmail.com>
2026-07-20 00:06:03 +0530

    Completed Week 3 Spring REST HandsOn4_REST_Get_All_Countries

---
+++ Week3/2. spring-rest-handson.docx/HandsOn4_REST_Get_All_Countries/src/main/java/com/cognizant/springlearn/controller/CountryController.java
@@ -0,0 +1,54 @@
+package com.cognizant.springlearn.controller;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cognizant.springlearn.Country;
+
+@RestController
+public class CountryController {
+
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(CountryController.class);
+
+    @RequestMapping("/country")
+    public Country getCountryIndia() {
+
+        LOGGER.info("START");
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("country.xml");
+
+        Country country =
+                context.getBean("in", Country.class);
+
+        LOGGER.info("END");
+
+        return country;
+    }
+
+    @SuppressWarnings("unchecked")
+    @GetMapping("/countries")
+    public List<Country> getAllCountries() {
+
+        LOGGER.info("START");
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("country.xml");
+
+        List<Country> countryList =
+                context.getBean("countryList", List.class);
+
+        LOGGER.info("END");
+
+        return countryList;
+    }
+
+}
