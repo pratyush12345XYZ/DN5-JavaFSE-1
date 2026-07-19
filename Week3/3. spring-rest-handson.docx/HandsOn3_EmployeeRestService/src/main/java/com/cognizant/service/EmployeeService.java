aa1986d8ee5c65bbea97e472ed68fd975b2a362c
Pratyush Kumar <pratyushmohanty12345@gmail.com>
2026-07-20 02:04:20 +0530

    Completed Week 3 Spring REST HandsOn3 Employee REST Service

---
+++ Week3/3. spring-rest-handson.docx/HandsOn3_EmployeeRestService/src/main/java/com/cognizant/service/EmployeeService.java
@@ -0,0 +1,21 @@
+package com.cognizant.service;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cognizant.dao.EmployeeDao;
+import com.cognizant.model.Employee;
+
+@Service
+public class EmployeeService {
+
+    @Autowired
+    private EmployeeDao employeeDao;
+
+    public ArrayList<Employee> getAllEmployees() {
+        return employeeDao.getAllEmployees();
+    }
+
+}
