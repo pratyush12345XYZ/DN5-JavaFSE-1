c988a9e01bc083449c2af041dd87d8d5718c4dc6
Pratyush Kumar <pratyushmohanty12345@gmail.com>
2026-07-20 00:30:53 +0530

    Completed Week 3 Spring REST HandsOn7_MockMVC_Test_Get_Country_Service

---
+++ Week3/2. spring-rest-handson.docx/HandsOn6_REST_Get_Country_Exceptional_Scenario/src/main/java/com/cognizant/springlearn/controller/CountryController.java
@@ -0,0 +1,74 @@
+package com.cognizant.springlearn.controller;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.cognizant.springlearn.service.exception.CountryNotFoundException;
+
+import com.cognizant.springlearn.Country;
+import com.cognizant.springlearn.service.CountryService;
+
+@RestController
+public class CountryController {
+
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(CountryController.class);
+
+    @Autowired
+    private CountryService countryService;
+
+    @RequestMapping("/country")
+    public Country getCountryIndia() {
+
+        LOGGER.info("START");
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("country.xml");
+
+        Country country =
+                context.getBean("in", Country.class);
+
+        LOGGER.info("END");
+
+        return country;
+    }
+
+    @SuppressWarnings("unchecked")
+    @GetMapping("/countries")
+    public List<Country> getAllCountries() {
+
+        LOGGER.info("START");
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("country.xml");
+
+        List<Country> countryList =
+                context.getBean("countryList", List.class);
+
+        LOGGER.info("END");
+
+        return countryList;
+    }
+
+    @GetMapping("/countries/{code}")
+    public Country getCountry(@PathVariable String code)
+            throws CountryNotFoundException {
+
+        LOGGER.info("START");
+
+        Country country = countryService.getCountry(code);
+
+        LOGGER.info("END");
+
+        return country;
+    }
+
+}
